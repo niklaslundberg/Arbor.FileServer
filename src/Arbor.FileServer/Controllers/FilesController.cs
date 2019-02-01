f4e75f7ed876c409812bcfe85175c6c146ecd667
Niklas Lundberg <info@nlundberg.se>
2019-02-01 14:44:17 +0100

    Added metadata endpoint.

---
+++ src/Arbor.FileServer/Controllers/FilesController.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Arbor.FileServer.Hashing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arbor.FileServer.Controllers
+{
+    public class FilesController : Controller
+    {
+        [Route("/file/{**filePath}")]
+        [HttpGet]
+        public object Index(string filePath, [FromServices] FileServerSettings settings)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                return new StatusCodeResult(400);
+            }
+
+            string fullFilePath = Path.Combine(settings.BasePath, filePath);
+
+            if (!System.IO.File.Exists(fullFilePath))
+            {
+                return new StatusCodeResult(404);
+            }
+
+            var fileInfo = new FileInfo(fullFilePath);
+
+            return new
+            {
+                fileInfo.Name,
+                fileInfo.Length,
+                fileInfo.LastAccessTimeUtc,
+                fileInfo.CreationTimeUtc,
+                fileInfo.LastWriteTimeUtc,
+                fileInfo.Extension
+            };
+        }
+    }
+}
